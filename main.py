5f4bc98e3362ecb5b73510a03ddee20112bd2688
C zao <zao@zf.local>
2023-10-03 15:38:57 +0200

    first project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
-
+user = {'username': 'Miguel'}
+# -*- coding: utf-8 -*-
 os.environ['FLASK_APP'] = 'main.py'
 
 app = Flask(__name__)
@@ -8,7 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'miguel'}
+    return '''<html>
+    <head>
+        <title>{{ Главная }} - Газон</title>
+    </head>
+    <body>
+        <h1>Hello, {{ user.username }}!</h1>
+    </body>
+</html>'''
 
 if __name__ == "__main__":
     app.run()
